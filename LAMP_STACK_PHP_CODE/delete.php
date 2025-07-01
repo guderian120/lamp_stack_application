e153210c364d40d38076359beefc0e9cd077b50b
guderian120 <realamponsah10@yahoo.com>
2025-07-01 12:04:59 +0000

    added ecr pipeline

--- LAMP_STACK_PHP_CODE/delete.php
+++ LAMP_STACK_PHP_CODE/delete.php
@@ -1,6 +1,12 @@
 <?php
 require_once 'config.php';
 
+// Check if the request is coming from index.php
+if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], 'index.php') === false) {
+    header("Location: index.php");
+    exit;
+}
+
 if (!isset($_GET['id'])) {
     header("Location: index.php");
     exit;
